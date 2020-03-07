c78afb29e26b23364503c013b8a05a7a6353e071
github-xihe <53401671+github-xihe@users.noreply.github.com>
2020-03-07 15:10:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-#add sonme additional paone
-git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/argon-mc
+#add luci
+git clone https://github.com/sypopo/luci-theme-argon-mc.git package/argon-mc
 ./scripts/feeds update -a
 ./scripts/feeds install -a
