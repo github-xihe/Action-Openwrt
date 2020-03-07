fe94913be11720cd16e7d552e69f43e3512fd1eb
github-xihe <53401671+github-xihe@users.noreply.github.com>
2020-03-07 14:22:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #add sonme additional packages 
 git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean
