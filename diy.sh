f1accd42ae8111ff977c3db187cf828e4c40321b
github-xihe <53401671+github-xihe@users.noreply.github.com>
2020-03-09 11:30:20 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,5 +9,3 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # 添加新的包
 git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
-# 删除默认密码
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
