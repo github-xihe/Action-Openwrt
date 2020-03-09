f1941968d9d2765af54ba7f3ef9ba024fc88a451
github-xihe <53401671+github-xihe@users.noreply.github.com>
2020-03-09 16:46:10 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # 添加新的包
 git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
