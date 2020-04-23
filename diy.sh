c2cf2e83d6d61faae1c1cd4870b24d71419ae3f0
github-xihe <53401671+github-xihe@users.noreply.github.com>
2020-04-23 17:59:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,5 +9,3 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 # 添加新的包
 git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
-# 取消bootstrap为默认主题
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
