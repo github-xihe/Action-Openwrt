dc9abced67264af3c7ed095aa66007fecc954d73
github-xihe <53401671+github-xihe@users.noreply.github.com>
2020-06-07 23:46:19 +0800

    Update diy.sh
    更改管理密码

--- diy.sh
+++ diy.sh
@@ -9,3 +9,5 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 # 添加新的包
 git clone https://github.com/sypopo/luci-theme-argon-mc.git package/lean/luci-theme-argon-mc
+#更改管理密码为@@@@@@@@
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$EeK6vzkj$QYe0YvZJseS4rwAcVh0Qy.:18377/g' package/lean/default-settings/files/zzz-default-settings
