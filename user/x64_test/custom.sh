2e5a4aa33fe8defbd46f35886895db266aaa5ae4
github-xihe <53401671+github-xihe@users.noreply.github.com>
2021-03-21 00:48:34 +0800

    Update custom.sh

--- user/x64_test/custom.sh
+++ user/x64_test/custom.sh
@@ -5,9 +5,12 @@
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
 
 # 添加软件包
-git clone https://github.com/garypang13/luci-app-bypass                     package/lean/luci-app-bypass
-git clone https://github.com/garypang13/smartdns-le                         package/lean/smartdns-le
-svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb   package/lean/lua-maxminddb
+git clone https://github.com/garypang13/luci-app-bypass                     package/diy/luci-app-bypass
+git clone https://github.com/garypang13/smartdns-le                         package/diy/smartdns-le
+svn co https://github.com/garypang13/openwrt-packages/trunk/chinadns-ng     package/diy/chinadns-ng
+svn co https://github.com/garypang13/openwrt-packages/trunk/trojan-go       package/diy/trojan-go
+svn co https://github.com/garypang13/openwrt-packages/trunk/trojan-plus     package/diy/trojan-plus
+svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb   package/diy/lua-maxminddb
 
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
