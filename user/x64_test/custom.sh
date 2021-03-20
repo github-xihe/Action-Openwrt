acdb1045f43edbef1f3d0d56bb6e5238d0fa4a67
github-xihe <53401671+github-xihe@users.noreply.github.com>
2021-03-21 00:04:00 +0800

    Create custom.sh

---
+++ user/x64_test/custom.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#=================================================
+# 切换内核
+#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
+#sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
