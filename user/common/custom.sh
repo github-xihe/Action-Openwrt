4825e2e586b0f53a5519ddf3ad9d51cf90f2b39d
github-xihe <53401671+github-xihe@users.noreply.github.com>
2021-03-21 00:05:26 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -5,6 +5,6 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #更改管理密码
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$EeK6vzkj$QYe0YvZJseS4rwAcVh0Qy.:18377/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$erud5AEi$5TlK2f6576IZ9r1E0pysq0:18688/g' package/lean/default-settings/files/zzz-default-settings
 
 #添加一些软件包
