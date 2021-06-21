7b2309a6897f357135d12ca621e193ce8e8cf85a
github-xihe <53401671+github-xihe@users.noreply.github.com>
2021-06-21 17:32:22 +0800

    Create custom.sh

---
+++ user/immortalwrt-x64/custom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#=================================================
+
+#修改默认IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+#更改管理密码
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$erud5AEi$5TlK2f6576IZ9r1E0pysq0:18688/g' package/emortal/default-settings/files/zzz-default-settings
