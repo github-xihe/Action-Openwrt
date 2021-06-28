df0e2be7cd21c00bd0d5ccf05c7060b9cd8ce47c
github-xihe <53401671+github-xihe@users.noreply.github.com>
2021-06-28 22:55:09 +0800

    Update custom.sh

--- user/x64_test/custom.sh
+++ user/x64_test/custom.sh
@@ -7,14 +7,6 @@
 # 添加软件包
 # luci-app-bypass
 #git clone https://github.com/garypang13/luci-app-bypass                     package/diy/luci-app-bypass
-#git clone https://github.com/garypang13/smartdns-le                         package/diy/smartdns-le
-#svn co https://github.com/garypang13/openwrt-packages/trunk/chinadns-ng     package/diy/chinadns-ng
-#svn co https://github.com/garypang13/openwrt-packages/trunk/trojan-go       package/diy/trojan-go
-#svn co https://github.com/garypang13/openwrt-packages/trunk/trojan-plus     package/diy/trojan-plus
-#svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb   package/diy/lua-maxminddb
-
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 
 # luci-app-openclash
 git clone https://github.com/vernesong/OpenClash.git                        package/diy/luci-app-openclash
