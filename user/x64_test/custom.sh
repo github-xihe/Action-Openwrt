74ae661faa8eeda90c839a1330c532b90fa4b807
github-xihe <53401671+github-xihe@users.noreply.github.com>
2021-07-09 00:44:59 +0800

    Update custom.sh

--- user/x64_test/custom.sh
+++ user/x64_test/custom.sh
@@ -5,8 +5,7 @@
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
 
 # 添加软件包
-# luci-app-bypass
-#git clone https://github.com/garypang13/luci-app-bypass                     package/diy/luci-app-bypass
-
-# luci-app-openclash
-git clone https://github.com/vernesong/OpenClash.git                        package/diy/luci-app-openclash
+#git clone https://github.com/garypang13/luci-app-bypass                    package/luci-app-bypass
+git clone https://github.com/vernesong/OpenClash.git                        package/luci-app-openclash
+git clone https://github.com/fw876/helloworld.git                           package/helloworld
+git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git   package/luci-app-unblockneteasemusic
