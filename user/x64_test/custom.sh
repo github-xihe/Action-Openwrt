ad8fd50975265c8abd1e1bbe87c5746aa210c5cd
github-xihe <53401671+github-xihe@users.noreply.github.com>
2021-09-19 18:36:07 +0800

    Update custom.sh

--- user/x64_test/custom.sh
+++ user/x64_test/custom.sh
@@ -5,6 +5,6 @@
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
 
 # 添加软件包
-git clone https://github.com/vernesong/OpenClash.git                            package/luci-app-openclash
-git clone https://github.com/fw876/helloworld.git                               package/helloworld
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic   package/luci-app-unblockneteasemusic
+git clone https://github.com/vernesong/OpenClash.git                                package/luci-app-openclash
+git clone https://github.com/fw876/helloworld.git                                   package/helloworld
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git   package/luci-app-unblockneteasemusic
