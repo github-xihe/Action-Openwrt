857882812c901f4a53fc043e7d4bfe1d1b5a671c
github-xihe <53401671+github-xihe@users.noreply.github.com>
2021-12-03 00:08:42 +0800

    Update custom.sh

--- user/immortalwrt-x64/custom.sh
+++ user/immortalwrt-x64/custom.sh
@@ -2,7 +2,7 @@
 #=================================================
 
 #修改默认IP
-sed -i 's/192.168.1.1/192.168.2.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #更改管理密码
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$erud5AEi$5TlK2f6576IZ9r1E0pysq0:18688/g' package/emortal/default-settings/files/99-default-settings
