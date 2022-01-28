be573c4ecde4a781d95459bbd91c118012f55ac7
github-xihe <53401671+github-xihe@users.noreply.github.com>
2022-01-29 00:59:27 +0800

    Update custom.sh

--- user/x64/custom.sh
+++ user/x64/custom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #=================================================
 # 切换内核
-#sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
 
 #添加一些软件包
 git clone https://github.com/fw876/helloworld.git                                    package/helloworld
