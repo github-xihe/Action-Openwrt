99b62f11211b626334b703a630ecfebc85a1fdca
github-xihe <53401671+github-xihe@users.noreply.github.com>
2022-01-30 21:22:07 +0800

    Update custom.sh

--- user/x64_test/custom.sh
+++ user/x64_test/custom.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #=================================================
 # 切换内核
-#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
-#sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
+#sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
 
 # 添加软件包
-git clone https://github.com/vernesong/OpenClash.git                                package/luci-app-openclash
+#git clone https://github.com/vernesong/OpenClash.git                                package/luci-app-openclash
 git clone https://github.com/fw876/helloworld.git                                   package/helloworld
 #git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git   package/luci-app-unblockneteasemusic
